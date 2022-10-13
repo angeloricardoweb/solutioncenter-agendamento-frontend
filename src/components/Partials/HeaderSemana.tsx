d27b3978af15bec2e1fcf4baa9ce40b8fd6fcf94
Angelo Ricardo <angeloengcomp@gmail.com>
2022-10-12 21:27:37 -0300

    feat(agendamento): logica de seleção do dia da semana

--- src/components/Partials/HeaderSemana.tsx
+++ src/components/Partials/HeaderSemana.tsx
@@ -1,15 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import { api } from '../../services/axios'
 
+interface HeaderSemanaProps {
+  month: string;
+  week: [
+    {
+      id: number;
+      day_of_week: string;
+      day_of_month: string;
+      enabled: boolean;
+    }
+  ]
+}
+
 export default function HeaderSemana() {
-  const [semana, setSemana] = useState({})
+  const [semana, setSemana] = useState<HeaderSemanaProps | undefined>()
+  // get current day of month
+  const dayCurrent = new Date().getDate().toString()
+  const [daySelected, setDaySelected] = useState(new Date().getDate().toString())
+
+  const btnEnabled = "flex flex-col items-center p-1 rounded-lg cursor-pointer bg-brand-blue-600";
+  const btnDisabled = "flex flex-col items-center p-1 rounded-lg cursor-pointer bg-zinc-300";
+  const btnActived = "ring-2 ring-green-600 ring-offset-2 rounded-lg";
+
 
   async function getDatasDaSemana() {
     try {
       const response = await api('http://localhost:3000/api/semana')
       console.log(response.data.results);
       setSemana(response.data.results)
-
     } catch (error) {
       console.log(error);
     }
@@ -19,6 +38,12 @@ export default function HeaderSemana() {
     getDatasDaSemana()
   }, [])
 
+  function handleSelectDay(day: string, enabled: boolean) {
+    if (enabled) {
+      setDaySelected(day)
+    }
+  }
+
 
   return (
     <div>
@@ -26,16 +51,17 @@ export default function HeaderSemana() {
 
       <div className='grid grid-cols-7 gap-3 mt-5'>
         {semana?.week.map(item => (
-          <div key={item.id} className='flex flex-col items-center'>
-            <span>
-              {item.day_of_week}
-            </span>
-            <span>
-              {item.day_of_month}
-            </span>
+          <div key={item.id} className={`${item.day_of_month == daySelected && btnActived}`} onClick={() => handleSelectDay(item.day_of_month, item.enabled)}>
+            <div className={item.enabled ? btnEnabled : btnDisabled}>
+              <span className='text-sm text-white'>
+                {item.day_of_week}
+              </span>
+              <span className='font-bold text-white'>
+                {item.day_of_month}
+              </span>
+            </div>
           </div>
         ))}
-
       </div>
     </div>
   )

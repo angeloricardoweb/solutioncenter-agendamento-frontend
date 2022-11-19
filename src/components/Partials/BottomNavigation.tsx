654f9a13181bcc4ebc763d3b103a6bb2760ad158
Angelo Ricardo <angeloengcomp@gmail.com>
2022-11-19 16:11:26 -0300

    fix(bottom navigatiom): add padding for iphone devices

--- src/components/Partials/BottomNavigation.tsx
+++ src/components/Partials/BottomNavigation.tsx
@@ -7,7 +7,7 @@ export default function ClientBottomNavigation({ active }: { active: string }) {
   const router = useRouter();
 
   return (
-    <div className="btm-nav shadow">
+    <div className="btm-nav shadow pb-3 h-[5rem]">
       <button className={active === "minha-conta" && "active"} onClick={()=>router.push('/cliente/minha-conta')}>
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
         <span className='text-sm'>Minha conta</span>

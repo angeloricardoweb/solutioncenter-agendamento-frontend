81cbc0afbd5827d6ce1e4368d4099a6354654403
Angelo Ricardo <angeloengcomp@gmail.com>
2022-11-27 21:24:25 -0300

    feat(horarios): gerencia de horarios

---
+++ src/pages/admin/opcoes/fechar-horarios/index.tsx
@@ -0,0 +1,81 @@
+import { parseCookies } from 'nookies';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import Admin from '../../../../components/Layouts/Admin'
+import HeaderPage from '../../../../components/Partials/HeaderPage'
+import { api } from '../../../../services/axios';
+import { toast } from 'react-hot-toast';
+
+
+
+export default function FecharHorarios() {
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm();
+
+  const { 'token': token } = parseCookies()
+
+
+  async function handleCloseRoom(data: any) {
+    const toasty = toast.loading('Fechando horários...')
+    try {
+      const response = await api.patch('/admin/agenda/gerencia-horarios', {
+        dia: data.dia,
+        horarios: data.horarios,
+        tipo: "fechar"
+      }, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      toast.success(response.data.message)
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+    toast.dismiss(toasty)
+  }
+
+  return (
+    <Admin title="opcoes">
+      <div className="main_container">
+        <HeaderPage title="Fechar horários" />
+        <h3 className=''>Selecione o dia e os horários que devem ser fechados.</h3>
+        <form onSubmit={handleSubmit(handleCloseRoom)} className="pb-24">
+
+          <div className='flex flex-col gap-2 mt-5'>
+            <div>
+              <label htmlFor="date" className='text-brand-blue-600'>Dia</label>
+
+              <input type="date" name="date" id="date" className='input input-bordered w-full'
+                {...register('dia', { required: true })}
+              />
+            </div>
+          </div>
+          <div className='mt-3'>
+            <span className='text-brand-blue-600 font-bold'>Horários</span>
+
+            {
+              ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"].map((item) => (
+                <label className="label cursor-pointer border rounded-lg mb-2" key={item}>
+                  <span className="label-text">{item}:00</span>
+                  <input value={item} type="checkbox" className="checkbox"
+                    {...register('horarios', { required: true })}
+                  />
+                </label>
+              ))
+            }
+
+          </div>
+          <div className='mt-3'>
+            <button className='btn btn-warning' disabled={isSubmitting}>Fechar horários</button>
+          </div>
+        </form>
+      </div>
+    </Admin>
+  )
+}
--- src/pages/admin/opcoes/index.tsx
+++ src/pages/admin/opcoes/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Admin from '../../../components/Layouts/Admin'
 import HeaderPage from '../../../components/Partials/HeaderPage'
+import ButtonLayout from './../../../components/Partials/ButtonLayout';
+import { useRouter } from 'next/router';
 
 export default function Opcoes() {
+  const router = useRouter()
   return (
     <Admin title="opcoes">
-       <div className="main_container">
-          <HeaderPage title="Opções" />
-          <h3 className='text-center'>Área em desenvolvimento</h3>
-       </div>
+      <div className="main_container">
+        <HeaderPage title="Opções" />
+        <h3 className='text-center'>Área em desenvolvimento</h3>
+        <div className='flex flex-col gap-2 mt-5'>
+          <ButtonLayout name={'Fechar horarios'} handler={() => router.push('/admin/opcoes/fechar-horarios')} />
+          <ButtonLayout name={'Abrir horarios'} handler={() => router.push('/admin/opcoes/abrir-horarios')} />
+        </div>
+      </div>
     </Admin>
   )
 }

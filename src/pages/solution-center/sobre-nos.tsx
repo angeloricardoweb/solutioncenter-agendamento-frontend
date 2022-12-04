b7252a4b4dfd0c2f1589a56b79c8e2392a540255
Angelo Ricardo <angeloengcomp@gmail.com>
2022-12-04 01:42:27 -0300

    feat(institucional): progress

---
+++ src/pages/solution-center/sobre-nos.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Admin from '../../components/Layouts/Admin'
+import HeaderPage from '../../components/Partials/HeaderPage'
+
+export default function SobreNos() {
+  return (
+    <section>
+      <div className="main_container">
+        <HeaderPage title="Sobre nós" />
+        <div className="mt-5">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident reprehenderit minus voluptas! Pariatur libero aliquid maiores illo, ipsam fuga consequatur quia dolorem? Pariatur, voluptates. Excepturi id provident velit labore minus.</p>
+        </div>
+      </div>
+    </section>
+  )
+}
---
+++ src/pages/solution-center/sobre-o-app.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Admin from '../../components/Layouts/Admin'
+import HeaderPage from '../../components/Partials/HeaderPage'
+
+export default function SobreOApp() {
+  return (
+    <section>
+      <div className="main_container">
+        <HeaderPage title="Sobre o App" />
+        <div className="mt-5">
+          <p>Este App foi desenvolvido por <br /> <strong>Angelo Ricardo Software</strong> </p>
+          <div>
+
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}

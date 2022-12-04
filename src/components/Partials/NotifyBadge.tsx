7bf040cfe1e7cc64d2a299f51fe505805b05dc21
Angelo Ricardo <angeloengcomp@gmail.com>
2022-12-04 15:35:58 -0300

    feat(cliente): done show notification

--- src/components/Partials/NotifyBadge.tsx
+++ src/components/Partials/NotifyBadge.tsx
@@ -1,14 +1,36 @@
 import { useRouter } from 'next/router';
-import React, { useState } from 'react'
+import { parseCookies } from 'nookies';
+import React, { useEffect, useState } from 'react'
+import { api } from '../../services/axios';
 
 export default function NotifyBadge() {
   const [notificationVisibility, setNotificationVisibility] = useState(true);
   const router = useRouter();
 
+  const { token: token } = parseCookies()
+
+  async function getNotificacoes() {
+    try {
+      const response = await api.get(`/notificacoes`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setNotificationVisibility(response.data.results.visualizada)
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(()=>{
+    getNotificacoes()
+  },[])
+
 
   return (
     <div className='grid justify-center'>
-      {notificationVisibility && (
+      {!notificationVisibility && (
         <div className="badge badge-accent animate-pulse" onClick={()=>router.push('/cliente/minha-conta/notificacoes')}>Nova notificação</div>
       )}
     </div>

19e2415f83015960169133c37325261225b9bd13
Angelo Ricardo <angeloengcomp@gmail.com>
2022-12-05 23:51:45 -0300

    feat(admin): add page historico

--- src/pages/admin/relatorios/index.tsx
+++ src/pages/admin/relatorios/index.tsx
@@ -5,10 +5,14 @@ import HeaderPage from '../../../components/Partials/HeaderPage'
 export default function Relatorios() {
   return (
     <Admin title="relatorios">
-       <div className="main_container">
-          <HeaderPage title="Relatórios" />
-          <h3 className='text-center'>Área em desenvolvimento</h3>
-       </div>
+      <div className="main_container">
+        <HeaderPage title="Relatórios" />
+        <h3 className='text-center'>Área em desenvolvimento</h3>
+        <div className="grid justify-center">
+          <hr />
+          <button className='btn btn-primary'>Gerar relatório</button>
+        </div>
+      </div>
     </Admin>
   )
 }

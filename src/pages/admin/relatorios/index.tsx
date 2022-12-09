730c5c019236a4b0a0ce386b787a7ed2c895039c
Angelo Ricardo <angeloengcomp@gmail.com>
2022-12-08 23:52:12 -0300

    feat(admin): show current month relatory

--- src/pages/admin/relatorios/index.tsx
+++ src/pages/admin/relatorios/index.tsx
@@ -1,17 +1,64 @@
-import React from 'react'
+import { parseCookies } from 'nookies'
+import React, { useEffect, useState } from 'react'
 import Admin from '../../../components/Layouts/Admin'
+import DescriptionBlock from '../../../components/Partials/DescriptionBlock'
 import HeaderPage from '../../../components/Partials/HeaderPage'
+import { api } from '../../../services/axios'
+interface IRelatory {
+  name: string;
+  mes_atual: string;
+  qtd_salas_reservadas: number;
+  qtd_salas_pagas: number;
+  qtd_salas_pendentes: number;
+  qtd_clientes: number;
+  qtd_clientes_ativo: number;
+  qtd_clientes_inativos: number;
+
+}
 
 export default function Relatorios() {
+  const [data, setData] = useState<IRelatory>()
+  const { token: token } = parseCookies()
+
+
+  async function getRelatorioGeral() {
+    try {
+      const response = await api.get('/admin/relatorios', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+
+      setData(response.data.results.main)
+
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    getRelatorioGeral()
+  }, [])
+
   return (
     <Admin title="relatorios">
       <div className="main_container">
         <HeaderPage title="Relatórios" />
-        <h3 className='text-center'>Área em desenvolvimento</h3>
-        <div className="grid justify-center">
-          <hr />
-          <button className='btn btn-primary'>Gerar relatório</button>
-        </div>
+          {
+            data && (
+              <>
+                <h3>{data.name}</h3>
+                <DescriptionBlock title={'Mês atual'} value={data.mes_atual}/>
+                <DescriptionBlock title={'Salas reservadas'} value={data.qtd_salas_reservadas.toString()}/>
+                <DescriptionBlock title={'Salas pagas'} value={data.qtd_salas_pagas.toString()}/>
+                <DescriptionBlock title={'Salas pendentes'} value={data.qtd_salas_pendentes.toString()}/>
+                <DescriptionBlock title={'Clientes'} value={data.qtd_clientes.toString()}/>
+                <DescriptionBlock title={'Clientes Ativos'} value={data.qtd_clientes_ativo.toString()}/>
+                <DescriptionBlock title={'Clientes Inativos'} value={data.qtd_clientes_inativos.toString()}/>
+              </>
+            )
+          }
       </div>
     </Admin>
   )
